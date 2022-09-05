e6c5d599827ed08b5842b0629435d338800ea253
max-x444 <maxfv1999@gmail.com>
2022-09-05 21:56:43 +0300

    implementation of lesson 26

--- project/src/main/java/com/Main.java
+++ project/src/main/java/com/Main.java
@@ -1,6 +1,5 @@
 package com;
 
-import com.config.MongoFactoryUtil;
 import com.model.constants.Manufacturer;
 import com.model.vehicle.Airplane;
 import com.model.vehicle.Auto;
@@ -12,6 +11,7 @@ import com.service.AutoService;
 import com.service.InvoiceService;
 import com.service.MotorbikeService;
 import lombok.SneakyThrows;
+import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +19,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
@@ -37,12 +36,13 @@ public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-        MongoFactoryUtil.connect("nix").drop();
         final Invoice invoice = new Invoice();
         invoice.setId(UUID.randomUUID().toString());
         invoice.setCreated(LocalDateTime.now());
         final Auto auto = AUTO_SERVICE.create(1).get(0);
+        System.out.println(AUTO_SERVICE.save(auto));
         System.out.println("Save auto: " + AUTO_SERVICE.save(auto));
+        System.out.println("Get first auto: " + AUTO_SERVICE.findById(auto.getId()));
         auto.setPrice(BigDecimal.valueOf(1_000.0));
         auto.setCount(1);
         auto.setBodyType("coupe");
@@ -56,10 +56,8 @@ public class Main {
         final Auto secondAuto = AUTO_SERVICE.create(1).get(0);
         secondAuto.setCount(1);
         secondAuto.setPrice(BigDecimal.valueOf(2_000.0));
-        secondAuto.setInvoice(secondInvoice);
         System.out.println("Save second auto: " + AUTO_SERVICE.save(secondAuto));
-//        System.out.println("Delete first auto: " + AUTO_SERVICE.delete(auto.getId()));
-//        System.out.println("Delete second auto: " + AUTO_SERVICE.delete(secondAuto));
+        secondAuto.setInvoice(secondInvoice);
         System.out.println("Get all autos: " + Arrays.toString(AUTO_SERVICE.getAll().toArray()));
         secondInvoice.setVehicles(Set.of(secondAuto));
         System.out.println("Save second invoice: " + INVOICE_SERVICE.save(secondInvoice));
@@ -71,8 +69,8 @@ public class Main {
                 BigDecimal.valueOf(500.0),
                 5,
                 1);
-        airplane.setInvoice(invoice);
         System.out.println("Save airplane: " + AIRPLANE_SERVICE.save(airplane));
+        airplane.setInvoice(invoice);
 
         final Motorbike motorbike = MOTORBIKE_SERVICE.create(
                 UUID.randomUUID().toString(),
@@ -85,8 +83,8 @@ public class Main {
                 "$",
                 new Engine(UUID.randomUUID().toString(), 3, "BMW"));
         motorbike.setDetails(List.of("wheels", "spoiler"));
-        motorbike.setInvoice(invoice);
         System.out.println("Save motorbike: " + MOTORBIKE_SERVICE.save(motorbike));
+        motorbike.setInvoice(invoice);
         System.out.println("Get motorbike: " + MOTORBIKE_SERVICE.findById(motorbike.getId()));
 
         invoice.setVehicles(Set.of(auto, airplane, motorbike));
@@ -95,14 +93,14 @@ public class Main {
         for (Invoice i : INVOICE_SERVICE.getAll()) {
             System.out.println(i);
         }
-        System.out.println("Get total count invoices: " + INVOICE_SERVICE.getTotalCountInvoices());
-        System.out.println("Group by total price:");
-        for (Map.Entry<BigDecimal, Long> entry : INVOICE_SERVICE.groupByTotalPrice().entrySet()) {
-            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
-        }
-        System.out.println("Get invoice more expensive than 1000:");
-        for (Invoice in : INVOICE_SERVICE.getInvoiceMoreExpensiveThanAmount(BigDecimal.valueOf(1000))) {
-            System.out.println(in);
-        }
+        System.out.println(INVOICE_SERVICE.delete(invoice.getId()));
+        System.out.println(INVOICE_SERVICE.delete(secondInvoice.getId()));
+
+        final Flyway flyway = Flyway.configure()
+                .dataSource(System.getenv("URL"), System.getenv("USER"), System.getenv("PASSWORD"))
+                .baselineOnMigrate(true)
+                .locations("db/migration")
+                .load();
+        flyway.migrate();
     }
 }
